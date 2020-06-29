ee529e3cc95172bbf1213cab6a322740e714fe77
Ingyue Han <abcolligh@naver.com>
2020-06-29 15:49:44 +0900

    오픈소스 과제 2번째 커밋 디지털 시계 구현

--- app/src/main/java/com/example/cpuwatch/MainActivity.java
+++ app/src/main/java/com/example/cpuwatch/MainActivity.java
@@ -3,12 +3,49 @@ package com.example.cpuwatch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView Show_Time_TextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Show_Time_TextView = findViewById(R.id.a);
+        ShowTimeMethod();
+    }
+
+    public void ShowTimeMethod(){
+        final Handler handler = new Handler(){
+            @Override
+            public void handleMessage(Message msg){
+                Show_Time_TextView.setText(DateFormat.getDateTimeInstance().format(new Date()));
+            }
+        };
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                while(true){
+                    try{
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    handler.sendEmptyMessage(1);
+                }
+            }
+        };
+        Thread thread = new Thread(task);
+        thread.start();
     }
 }
